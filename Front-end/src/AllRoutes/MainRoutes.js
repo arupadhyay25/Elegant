1a144c88b3bcfce78797e0fdf415f9ce2b54a320
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-12-15 22:44:41 +0530

    day-3 work

--- Front-end/src/AllRoutes/MainRoutes.js
+++ Front-end/src/AllRoutes/MainRoutes.js
@@ -8,6 +8,8 @@ import { Womens } from "../Pages/Womens";
 import { Accessories } from "../Pages/Accessories";
 import { Mobilecovers } from "../Pages/Mobilecovers";
 import { Winterwear } from "../Pages/Winterwear";
+import { FansPage } from "../Pages/FansPage";
+import DownloaduiPage from "../Pages/DownloaduiPage";
 
 export const MainRoutes = () => {
   return (
@@ -20,6 +22,8 @@ export const MainRoutes = () => {
       <Route path="/accessories" element={<Accessories />} />
       <Route path="/mobile-care" element={<Mobilecovers />} />
       <Route path="/winter-wear" element={<Winterwear />} />
+      <Route path="/fanspage" element={<FansPage />} />
+      <Route path="/downloaduipage" element={<DownloaduiPage />} />
     </Routes>
   );
 };
