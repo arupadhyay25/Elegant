48d0d4303d8e5d59d28ab976a4aa865a75d4a9ea
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-12-17 00:09:03 +0530

    work done

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -4,16 +4,18 @@ import { Footer } from "./Components/Footer";
 import { Navbar } from "./Components/Navbar";
 import DownloaduiPage from "./Pages/DownloaduiPage";
 import FansPage from "./Pages/FansPage";
+import Offers from "./Pages/Offers";
 
 function App() {
   return (
     <div className="App">
-      <br />
+      {/* <br />
       <Navbar />
       <br />
       <MainRoutes />
       <br />
-      <Footer />
+      <Footer /> */}
+      <Offers />
     </div>
   );
 }
---
+++ Front-end/src/Pages/Offers.jsx
@@ -0,0 +1,35 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
+import React from "react";
+
+function Offers(props) {
+  return (
+    <div>
+      <Flex
+        flexDirection={"column"}
+        px="10%"
+        py="1%"
+        justifyContent={"center"}
+        alignItems={"center"}
+        border="1px solid red"
+      >
+        <Text
+          fontSize={"48px"}
+          fontFamily="HelveticaNeue-Bold"
+          fontWeight="bold"
+        >
+          Bewakoof Offers
+        </Text>
+        <Text color={"rgba(0,0,0,0.9)"} fontWeight="600" fontSize={"20px"}>
+          Find the best offers across our platforms on this page.
+        </Text>
+      </Flex>
+      <Flex flexDirection={"column"} gap="10px" w="80%" m="auto">
+        <Image src="https://images.bewakoof.com/uploads/grid/app/bewakoof-desktop-1440---x-150---tribe-1625739880.png" />
+        <Image src="https://images.bewakoof.com/uploads/grid/app/bewakoof-desktop-1440---x-150---prep10-1625739466.png" />
+        <Image src="https://images.bewakoof.com/uploads/grid/app/thin-strip-banner-desktop-freecharge-1664523917.png" />
+      </Flex>
+    </div>
+  );
+}
+
+export default Offers;
