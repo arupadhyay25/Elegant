6e642d9fbade42f8681c7f85c225883e0938c897
nileshsingh72 <rnileshchauhan89@gmail.com>
2022-12-17 17:11:15 +0530

    updating

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -2,14 +2,10 @@ import { MainRoutes } from "./AllRoutes/MainRoutes";
 import "./App.css";
 import { Footer } from "./Components/Footer";
 import { Navbar } from "./Components/Navbar";
-import DownloaduiPage from "./Pages/DownloaduiPage";
-import FansPage from "./Pages/FansPage";
-import Offers from "./Pages/Offers";
 
 function App() {
   return (
     <div className="App">
-      
       <br />
       <Navbar />
       <br />
